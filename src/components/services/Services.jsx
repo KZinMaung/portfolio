9344289a8d3f4eb0007e16d1579e06feddcdf686
KayZinMaung1 <kayzinmg363@gmail.com>
2022-10-18 15:08:45 +0630

    section - nav, about, experience, services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,112 @@
 import React from 'react'
-
+import { BiCheck } from 'react-icons/bi';
+import './services.css';
 const Services = () => {
     return (
-        <div>Services</div>
+        <section id="services">
+            <h5>What I Offer</h5>
+            <h2>Services</h2>
+            <div className='container services__container'>
+                <article className='service'>
+                    <div className="service__head">
+                        <h3>UI/UX Design</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                    </ul>
+                </article>
+                <article className='service'>
+                    <div className="service__head">
+                        <h3>Web Development</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                    </ul>
+                </article>
+                <article className='service'>
+                    <div className="service__head">
+                        <h3>Content Creation</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                        <li>
+                            <BiCheck class='service__list-icon' />
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                        </li>
+                    </ul>
+                </article>
+
+            </div>
+        </section>
     )
 }
 
